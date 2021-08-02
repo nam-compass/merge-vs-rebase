1f93d088505d0dd9bb8ed6473bbc9aa5c21ede52
Nam Nguyen <nam.nguyen@compass.com>
2021-08-02 09:38:03 -0400

    Update 2.js from rebase branch

--- 2.js
+++ 2.js
@@ -1,6 +1,6 @@
 console.log("This");
 console.log("prints update from main");
-console.log("something");
+console.log("something from rebase branch");
 console.log("really");
 console.log("fancy");
 console.log("New line from main branch");
